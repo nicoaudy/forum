c26cc7af7e960eede1bd52d0f2183fa398db13e7
nicoaudy <nicoaudy1994@gmail.com>
2017-08-09 22:19:42 +0700

    ++signup form

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    SignUp
+    <form autocomplete="off">
+    	<div class="form-group">
+    		<label for="email">Email</label>
+    		<input type="email" id="email" class="form-control" placeholder="you@email.com" required>
+    	</div>    	
+    	<div class="form-group">
+    		<label for="username">Username</label>
+    		<input type="text" id="username" class="form-control" required>
+    	</div>
+    	<div class="form-group">
+    		<label for="password">Password</label>
+    		<input type="password" id="password" class="form-control" required>
+    	</div>
+    	<button type="submit" class="btn btn-default pull-right">Signup</button>
+    </form>
   </div>
 </template>
 
