6fa925208c58a045dab6b97e1c2e3eaf005f96be
johndoe <nicoaudy1994@gmail.com>
2017-08-10 15:03:52 +0700

    save token to local storage and signin functionality

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import axios from 'axios'
 
 export default {
+  signin(context, email, password){
+    axios.post('/api/auth/signin', {
+      email: email,
+      password: password
+    }).then(response => {
+      context.error = false
+      localStorage.setItem('id_token', response.data.meta.token)
+    }, response => {
+      context.error = true
+    })
+  },
 	signup(context, username, email, password){
         axios.post('/api/auth/signup', {
            username: username, 
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,11 +1,39 @@
 <template>
   <div>
-    SignIn
+    <div class="alert alert-danger" v-if="error">
+        <p>Could not sign you in with those details.</p>
+    </div>
+	<form autocomplete="off" v-on:submit.prevent="signin">
+    	<div class="form-group">
+    		<label for="email">Email</label>
+    		<input type="email" id="email" v-model="email" class="form-control" placeholder="you@email.com" required>
+    	</div>
+    	<div class="form-group">
+    		<label for="password">Password</label>
+    		<input type="password" id="password" v-model="password" class="form-control" required>
+    	</div>
+    	<button type="submit" class="btn btn-default pull-right">Signin</button>
+    </form>
   </div>
 </template>
 
 <script>
+import auth from '../auth'
+
 export default {
-	
+    data() {
+        return {
+            email: null,
+            password: null,
+            success: false,
+            error: false,
+            response: null
+        }
+    },
+	methods: {
+        signin(){
+            auth.signin(this, this.email, this.password)
+        }
+    }
 }
 </script>
